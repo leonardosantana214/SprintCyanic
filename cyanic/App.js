f7fa0f6efe72900bcafe01cf1cd1a6a7ae16f7f0
Leonardo Cardoso Ferreira de Santana <leonardocardo00@gmail.com>
2024-06-06 14:46:20 -0300

    Ex...

--- cyanic/App.js
+++ cyanic/App.js
@@ -4,34 +4,28 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './components/Login'; 
 import SplashScreen from './components/splash'; 
-import firebaseConfig from './components/firebase';
+// import firebaseConfig from './components/firebase';
 import TrainingCenterScreen from './components/default';
 import MenuBar from './components/homeBar';
 import Lista from './components/lista';
 import CalcScreen from './components/calc';
+import IndexExercicios from './components/Exercicies/app';
 
 
 const Stack = createStackNavigator();
 
-class App extends Component {
+class AppL extends Component {
   render(){
     return (
       <NavigationContainer>
-        {/* <Stack.Navigator initialRouteName="SplashScreen" screenOptions={{ headerShown: false }}>
+        <Stack.Navigator initialRouteName="SplashScreen" screenOptions={{ headerShown: false }}>
           <Stack.Screen name="SplashScreen" component={SplashScreen} />
-          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="IndexExercicios" component={IndexExercicios} />
         </Stack.Navigator>
-        <TrainingCenterScreen /> */}
-        <CalcScreen />
+ 
       </NavigationContainer>
     );
   }
 }
 
-export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+export default AppL;
